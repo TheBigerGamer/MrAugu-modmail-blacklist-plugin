9c39bd2f0cd95034da8777280a9d62882a1d2133
TheBigerGamer Oficial <thebigergameroficial@gmail.com>
2020-04-25 17:23:19 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -10,7 +10,7 @@ module.exports = class {
    let response = await db.fetch(`bl_response`)
    if (!response) await db.set(`bl_response`, `This can be any string, however, there are some options:\n >{{user}} - The username of the user.\n >{{reason}} - The reason of the blacklist.\n >{{moderator}} - Who blacklisted.\n {{prefix}} - Prefix of the server.\n Use \`{{prefix}}blConfig\` to change this.`)
    //if (response !== 'bl_response', `This can be any string, and you can add some options:\n >{{user}} - The username of the user.\n >{{reason}} - The reason of the blacklist.\n >{{moderator}} - Who blacklisted.\n Use \`{{prefix}}blConfig\` to change this.`)
-   response.replace('{{user}}', message.author.username).replace('{{reason}}', userData.reason).replace('{{moderator}}', userData.moderator).replace('{{prefix}}',this.client.config.prefix)
+   if (userData) response.replace('{{user}}', message.author.username).replace('{{reason}}', userData.reason).replace('{{moderator}}', userData.moderator).replace('{{prefix}}',this.client.config.prefix)
    if (userData) return message.channel.send(response)
   }
 }
